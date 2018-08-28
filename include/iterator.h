26b074c5fd60a9bc4deb473cbb12536e934bf1d7
jeafleohj <jhony.angulo@utec.edu.pe>
2018-08-28 06:18:19 -0500

    Implement itertor's functions

--- include/iterator.h
+++ include/iterator.h
@@ -5,17 +5,48 @@
 
 template <typename T>
 class Iterator {
-    private:
-        Node<T>* current;
+private:
+	Node<T>* current;
     
-    public:
-        Iterator();
-        Iterator(Node<T>* node);
+public:
+	Iterator();
+	Iterator(Node<T>* node);
          
-        Iterator<T> operator=(Iterator<T> node);
-        bool operator!=(Iterator<T> cmp);
-        Iterator<T> operator++();
-        T operator*();
+	Iterator<T> operator=(Iterator<T> node);
+	bool operator!=(Iterator<T> cmp);
+	Iterator<T> operator++();
+	T operator*();
 };
 
+template <typename T>
+Iterator<T>::Iterator(){
+	current = nullptr;
+}
+
+template <typename T>
+Iterator<T>::Iterator(Node<T>* node):
+	current(node)
+{}
+
+template <typename T>
+Iterator<T> Iterator<T>::operator=(Iterator<T> node){
+	this = &node;
+	return *this;
+}
+
+template <typename T>
+bool Iterator<T>::operator!=(Iterator<T> cmp){
+	return (current != cmp.current);
+}
+
+template <typename T>
+Iterator<T> Iterator<T>::operator++(){
+	current = current -> next;
+	return *this;
+}
+
+template <typename T>
+T Iterator<T>::operator*(){
+	return current->data;
+}
 #endif
